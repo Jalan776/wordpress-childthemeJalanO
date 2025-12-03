5c9c7dacee5685e79c38713543fdf592ab49669b
Jalan776 <jalanollivierre@gmail.com>
2025-12-03 08:56:48 -0500

    Updated functions.php to enqueue child theme styles

--- twentytwentyfive-child-JalanO/functions.php
+++ twentytwentyfive-child-JalanO/functions.php
@@ -4,3 +4,33 @@ function jalan_enqueue_styles() {
     wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
 }
 add_action('wp_enqueue_scripts', 'jalan_enqueue_styles');
+
+
+/* --
+   1. Enqueue custom-style.css (styles for front-page + single)
+   ---- */
+function jalan_custom_styles() {
+    wp_enqueue_style(
+        'custom-style',
+        get_stylesheet_directory_uri() . '/custom-style.css',
+        array(),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action('wp_enqueue_scripts', 'jalan_custom_styles');
+
+
+/* 
+   2. Enqueue logged-in.css (ONLY for logged-in users)
+    */
+function jalan_logged_in_styles() {
+    if ( is_user_logged_in() ) {
+        wp_enqueue_style(
+            'logged-in-css',
+            get_stylesheet_directory_uri() . '/logged-in.css',
+            array(),
+            wp_get_theme()->get('Version')
+        );
+    }
+}
+add_action('wp_enqueue_scripts', 'jalan_logged_in_styles');
